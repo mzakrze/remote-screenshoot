5d85a11eeb6faec2d621373c5868dd13c2fe8db6
Mariusz Zakrzewski <mzakrze1@gmail.com>
2019-05-17 16:33:46 +0200

    App generated with Spring Initializr

---
+++ src/main/java/pl/mzakrze/remotescreenshot/RemoteScreenshotApplication.java
@@ -0,0 +1,13 @@
+package pl.mzakrze.remotescreenshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteScreenshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteScreenshotApplication.class, args);
+	}
+
+}
